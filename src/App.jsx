a9185c96ded67e86a714df058a25d7be402c459e
اِسْمَةُ الله بختيرو <139554680+baxt1y0rov@users.noreply.github.com>
2024-09-09 20:08:17 +0500

    1

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>Project in progress...</p>
+      <h1>AIFAN</h1>
+      <h3>Contact:</h3>
+      <a href="https://t.me/@baxt1y0rov">Developer</a>
+      <a href="https://t.me/@aiuzbe">Visit the Channel</a>
+    </>
+  )
+}
+
+export default App
